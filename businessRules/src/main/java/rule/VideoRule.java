d5fbdd4b6e39c1860343ce484129492921b2f31d
shahd <dimple.shah@tomtom.com>
2021-11-29 19:52:18 +0530

    Adding rule for Learning to SKi VIdeo

--- businessRules/src/main/java/rule/VideoRule.java
+++ businessRules/src/main/java/rule/VideoRule.java
@@ -2,12 +2,20 @@ package rule;
 
 import model.Action;
 import model.Product;
+import model.ProductType;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class VideoRule implements IBusinessRule{
     @Override
     public List<Action> execute(Product product) {
-        return null;
+        List<Action> actions = new ArrayList<>();
+        if (product.getProductType().equals(ProductType.VIDEO)){
+            if (product.getProductName().equals("Learning to Ski")){
+                actions.add(Action.ADD_FREE_FIRST_AID_VIDEO);
+            }
+        }
+        return actions;
     }
 }
