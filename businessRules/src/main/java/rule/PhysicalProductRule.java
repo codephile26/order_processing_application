8039e461dc5d5f98fcecebd8a75f2e25f2b0f912
shahd <dimple.shah@tomtom.com>
2021-11-29 19:48:00 +0530

    Added physical product rule - to generate packing slip for the shipping department and to send commission to agent

--- businessRules/src/main/java/rule/PhysicalProductRule.java
+++ businessRules/src/main/java/rule/PhysicalProductRule.java
@@ -2,12 +2,19 @@ package rule;
 
 import model.Action;
 import model.Product;
+import model.ProductCategory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PhysicalProductRule implements IBusinessRule{
     @Override
     public List<Action> execute(Product product) {
-        return null;
+        List<Action> actions = new ArrayList<>();
+        if (product.getProductCategory().equals(ProductCategory.PHYSICAL)){
+            actions.add(Action.GENERATE_PACKING_SLIP_FOR_SHIPPING);
+            actions.add(Action.GENERATE_A_PAY_COMMISSION);
+        }
+        return actions;
     }
 }
