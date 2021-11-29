6ea364e3a0c18116746ecbd58bc3ee4999c6e50d
shahd <dimple.shah@tomtom.com>
2021-11-29 20:04:39 +0530

    added class to determine actions to be taken for a particular product

--- businessRules/src/main/java/service/ActionDeterminerService.java
+++ businessRules/src/main/java/service/ActionDeterminerService.java
@@ -1,6 +1,30 @@
 package service;
 
+import model.Action;
+import model.Product;
+import rule.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ActionDeterminerService {
 
+    List<IBusinessRule> rules = new ArrayList<>();
 
+    public ActionDeterminerService() {
+        rules.add(new PhysicalProductRule());
+        rules.add(new NonPhysicalProductRule());
+        rules.add(new BookRule());
+        rules.add(new MembershipRule());
+        rules.add(new UpgradeMembershipRule());
+        rules.add(new VideoRule());
+    }
+
+    public List<Action> findTheActionsForProduct(Product product){
+        List<Action> actions = new ArrayList<>();
+        for (IBusinessRule rule : rules) {
+            actions.addAll(rule.execute(product));
+        }
+        return actions;
+    }
 }
