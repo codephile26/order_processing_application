247ee4d524c8cb61380d6f2b88f4ce6a5b2b2942
shahd <dimple.shah@tomtom.com>
2021-11-29 19:48:58 +0530

    Send email to owner for membership/activation or upgrade. membership/upgrade are classified as NONPHYSICAL Products

--- businessRules/src/main/java/rule/NonPhysicalProductRule.java
+++ businessRules/src/main/java/rule/NonPhysicalProductRule.java
@@ -2,12 +2,18 @@ package rule;
 
 import model.Action;
 import model.Product;
+import model.ProductCategory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NonPhysicalProductRule implements IBusinessRule{
     @Override
     public List<Action> execute(Product product) {
-        return null;
+        List<Action> actions = new ArrayList<>();
+        if (product.getProductCategory().equals(ProductCategory.NONPHYSICAL)){
+            actions.add(Action.SEND_EMAIL_TO_OWNER);
+        }
+        return actions;
     }
 }
